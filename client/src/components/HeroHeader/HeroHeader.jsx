607b585c8941bd5627b0c928ea2a4514418f004f
David Nugent <davienugent@hotmail.com>
2021-04-30 16:33:14 -0700

    Updates to initial folder structure & React Router - Began adding placeholder pages for the diffeent sections of site - Added Initial set-up of react router and url's

---
+++ client/src/components/HeroHeader/HeroHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HeroHeader() {
+  return (
+    <div>
+      <p>I can be your hero baby</p>
+    </div>
+  )
+}
+
+export default HeroHeader
---
+++ client/src/pages/TopicContent/TopicContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TopicContent() {
+  return (
+    <div>
+      <h2>Topic Content</h2>
+    </div>
+  )
+}
+
+export default TopicContent
