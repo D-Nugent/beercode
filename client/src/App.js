7f5076b3da1687eef0ee76632970c55c8e73491c
David A. Nugent <73457386+D-Nugent@users.noreply.github.com>
2021-04-30 16:51:28 -0700

    Development (#1)
    * Updates to initial folder structure & React Router
    - Began adding placeholder pages for the diffeent sections of site
    - Added Initial set-up of react router and url's
    
    * Added README

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import './App.scss';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import Home from './pages/Home/Home'
+import TopicIntro from './pages/TopicIntro/TopicIntro'
+import TopicContent from './pages/TopicContent/TopicContent'
+import About from './pages/About/About'
+import Contact from './pages/Contact/Contact'
+import HeroHeader from './components/HeroHeader/HeroHeader'
 
 function App() {
   return (
     <div className="app">
       <h1>BeerCode</h1>
+      <HeroHeader/>
+      <Switch>
+        <Redirect exact from="/" to="/home"/>
+        <Route path="/home" render={(routeProps) => <Home {...routeProps}/>}/>
+        <Route exact path="/tutorials/:topic" render={(routeProps) => <TopicIntro {...routeProps}/>}/>
+        <Route path="/tutorials/:topic/:topicid" render={(routeProps) => <TopicContent {...routeProps}/>}/>
+        <Route path="/about" render={(routeProps) => <About {...routeProps}/>}/>
+        <Route path="/contact" render={(routeProps) => <Contact {...routeProps}/>}/>
+      </Switch>
     </div>
   );
 }
---
+++ client/src/pages/TopicIntro/TopicIntro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TopicIntro() {
+  return (
+    <div>
+      <h3>Topic Intro</h3>
+    </div>
+  )
+}
+
+export default TopicIntro
