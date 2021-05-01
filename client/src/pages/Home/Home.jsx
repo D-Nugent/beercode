963b97107c8459e95de159a6fe77c1f973ec03ab
David Nugent <davienugent@hotmail.com>
2021-05-01 16:38:31 -0700

    Header component added, initial welcome page content added.

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,7 +4,17 @@ import './Home.scss'
 function Home() {
   return (
     <main className="home">
-      <h2 className="home__welcome">Welcome,</h2>
+      <h2 className="home__welcome">Welcome to BEERCODE</h2>
+      <p className="home__content">
+        Created by David Nugent, a developer based in British Columbia, Canada. Beercode
+        seeks to provide free web development tutorials on a variety of topics and 
+        frameworks with a focus on javascript.
+      </p>
+      <p className="home__content">
+        Should you have any questions about the content or if you would like to request 
+        tutorials on any specific content please feel free to contact the team through the 
+        'Contact' page.
+      </p>
     </main>
   )
 }
