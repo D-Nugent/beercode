a555f3995a1c30a53214a7cc27080a4ac17879c2
David Nugent <davienugent@hotmail.com>
2021-05-04 00:17:07 -0700

    Content Update, Logic Update - Added custom favicon - Updated Hero to link to Home - Added prelim content for TopicIntro, TutorialCards and TopicContent

--- client/src/components/InlineNav/InlineNav.jsx
+++ client/src/components/InlineNav/InlineNav.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import './InlineNav.scss';
 
 function InlineNav({tutorialData}) {
-  console.log(tutorialData);
   return (
     <nav className="inline-nav">
-      <h3 className="inline-nav__title">Tutorial Topics:</h3>
       {tutorialData.map(topic => {
         const {topicName,topicId} = topic;
         return(
-          <Link 
+          <NavLink 
           className="inline-nav__link"
+          activeClassName="inline-nav__link--active"
           to={`/tutorials/${topicName}`} 
           key={topicId}>
             {topicName}
-          </Link>
+          </NavLink>
         )
       })}
     </nav>
--- client/src/pages/TopicContent/TopicContent.jsx
+++ client/src/pages/TopicContent/TopicContent.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react';
+import {Link,useParams} from 'react-router-dom';
+import './TopicContent.scss';
 
-function TopicContent() {
+function TopicContent({tutorialData}) {
+  let params = useParams();
+  const [selectedTutorial,setSelectedTutorial] = useState({loaded: false,tutorialDetails:{}})
+
+console.log(selectedTutorial);
+  useEffect(() => {
+    let topic = tutorialData.find(topic => topic.topicName===params.topic) || []
+    setSelectedTutorial({
+      loaded: true,
+      tutorialDetails: topic.tutorials.find(tutorial => tutorial.videoId===params.topicId) || []
+    })
+  }, [params,tutorialData])
+
   return (
-    <div>
-      <h2>Topic Content</h2>
-    </div>
+    <section className="topic-content">
+      <Link className="topic-content__back-link"
+        to={`/tutorials/${params.topic}`}
+      >
+        Go Back
+      </Link>
+      {selectedTutorial.loaded===true &&
+      <>
+        <h2 className="topic-content__title">
+          {selectedTutorial.tutorialDetails.videoTitle}
+        </h2>
+        <video src="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4" 
+          className="topic-content__video-player"
+          controls
+        />
+        <p className="topic-content__video-details">
+          This is an example of the type of video details that you might have
+          underneath a tutorial video. Here there will be a brief description
+          of what the video covers and links to resources as needed.
+        </p>
+      </>
+      }
+    </section>
   )
 }
 
